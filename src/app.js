d497e5391f3e1ae47d612ab22cbbad1698966343
Emmanuel0085566-github <emmanuelniyi1997@gmail.com>
2026-08-03 11:14:40 +0100

    Allowing live url

--- src/app.js
+++ src/app.js
@@ -17,7 +17,11 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: [
+    "http://localhost:3000",
+    "https://create-your-shop.vercel.app",
+    /\.vercel\.app$/,
+  ],
   credentials: true,
 }))
 
